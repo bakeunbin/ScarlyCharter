b4fc70f3f83a5bf0dff4930432de48a56251ed14
Eunbin Bak <peb3978@gmail.com>
2021-04-16 21:15:16 -0400

    Changed to MVC style; changed to .NET Core 5.0.

--- ScarlyCharter/Startup.cs
+++ ScarlyCharter/Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ScarlyCharter.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,12 +23,7 @@ namespace ScarlyCharter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext> (options =>
-                 options.UseSqlServer (
-                     Configuration.GetConnectionString ("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser> (options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext> ();
-            services.AddRazorPages ();
+            services.AddControllersWithViews ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,26 +32,25 @@ namespace ScarlyCharter
             if (env.IsDevelopment ())
             {
                 app.UseDeveloperExceptionPage ();
-                app.UseDatabaseErrorPage ();
             }
             else
             {
-                app.UseExceptionHandler ("/Error");
+                app.UseExceptionHandler ("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts ();
             }
-
             app.UseHttpsRedirection ();
             app.UseStaticFiles ();
 
             app.UseRouting ();
 
-            app.UseAuthentication ();
             app.UseAuthorization ();
 
             app.UseEndpoints (endpoints =>
              {
-                 endpoints.MapRazorPages ();
+                 endpoints.MapControllerRoute (
+                     name: "default",
+                     pattern: "{controller=Home}/{action=Index}/{id?}");
              });
         }
     }
