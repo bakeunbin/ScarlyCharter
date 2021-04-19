bb9e4107a9439578af186c8ddc453ecf702e4d48
Eunbin Bak <peb3978@gmail.com>
2021-04-18 21:12:11 -0400

    Implemented Scheduling system. Currently very buggy.

--- ScarlyCharter/Controllers/HomeController.cs
+++ ScarlyCharter/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ScarlyCharter.Data;
 using ScarlyCharter.Models;
+using System.Globalization;
 
 namespace ScarlyCharter.Controllers
 {
@@ -29,9 +30,80 @@ namespace ScarlyCharter.Controllers
             return View ();
         }
 
-        public IActionResult Schedule ()
+        public IActionResult BookTrip (string guidestr, string locationstr, string fishstr, int partySize, string datestr)
         {
-            return View ();
+            DateTime parsed;
+
+            if (partySize < 1)
+                return Redirect ("/Home/Schedule?error=2");
+
+            if (!DateTime.TryParseExact (datestr, "MM/dd/yyyy hh:mm", new CultureInfo ("en-US"), DateTimeStyles.None, out parsed))
+                return Redirect ("/Home/Schedule?error=1");
+
+            var db = new ApplicationDbContext ();
+            var schedule = new Schedule
+            {
+                ScheduleId = db.Schedules.Count (),
+                Date = parsed,
+                StartTime = parsed.TimeOfDay,
+                EndTime = parsed.TimeOfDay.Add (new TimeSpan (8, 0, 0))
+            };
+
+            db.Schedules.Add (schedule);
+
+            var guide = (from g in db.Guides
+                         where g.GuideName.Equals (guidestr)
+                         select g).First ();
+
+            var location = (from l in db.Locations
+                            where l.Type == locationstr && l.RegionId == guide.RegionId
+                            select l).First ();
+
+            var fish = (from f in db.Fish
+                        where f.FishName.Equals (fishstr)
+                        select f).First ();
+
+            var booked_trip = new BookedTrip
+            {
+                TripId = db.BookedTrips.Count (),
+                ClientId = 0,
+                GuideId = guide.GuideId,
+                ScheduleId = schedule.ScheduleId,
+                LocationId = location.LocationId,
+                TargetFishId = fish.FishId,
+                PartySize = partySize,
+                FishingStyle = guide.FishingStyle
+            };
+
+            db.BookedTrips.Add (booked_trip);
+
+            db.SaveChanges ();
+
+            return Redirect ("/Home/Schedule?error=0");
+        }
+
+        public IActionResult Schedule (string guide, string location, int? partySize, string date, int? error)
+        {
+            var db = new ApplicationDbContext ();
+            var model = new ScheduleViewModel
+            {
+                Error = error ?? 0,
+                Guide = "",
+                Location = !string.IsNullOrEmpty (location) ? location : "",
+                PartySize = (int) (partySize != null ? partySize : 1),
+                Date = !string.IsNullOrEmpty (date) ? date : ""
+            };
+
+            if (!string.IsNullOrEmpty (guide))
+            {
+                var gid = (from g in db.Guides
+                          where g.GuideName.Equals (guide)
+                          select g).First ();
+
+                model.Guide = guide;
+            }
+
+            return View (model);
         }
 
         public IActionResult Search (string region, string season, string fish)
---
+++ ScarlyCharter/Models/ScheduleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScarlyCharter.Models
+{
+    public class ScheduleViewModel
+    {
+        public int Error;
+        public string Guide;
+        public string Location;
+        public int PartySize;
+        public string Date;
+    }
+}
