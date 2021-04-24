ba464cd34d1e01fc4e12ab429f3ed9006402c5c2
Eunbin Bak <peb3978@gmail.com>
2021-04-24 15:12:53 -0400

    Working on IdentityCore.

---
+++ ScarlyCharter/Areas/Identity/Data/ScarlyCharterUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ScarlyCharter.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ScarlyCharterUser class
+    public class ScarlyCharterUser : IdentityUser
+    {
+    }
+}
